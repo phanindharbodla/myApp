78c82ace0cae2455cd77fd66310cb40c8fdf327a
Phanindhar Bodla <pbodla@factset.com>
2017-01-09 15:40:35 +0530

    eval mode implemented

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,42 +1,52 @@
 angular.module('app.controllers', [])
-  
-.controller('lengthsAndPlotsConvertorCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+  .controller('lengthsAndPlotsConvertorCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    function($scope, $stateParams) {
 
 
-}])
-   
-.controller('aboutCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    }])
+
+  .controller('aboutCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    function($scope, $stateParams) {
 
 
-}])
-   
-.controller('expressionModeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    }])
+
+  .controller('expressionModeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    function($scope, $stateParams) {
 
+      $scope.form = {query: ''}
+      $scope.update = function() {
 
-}])
-   
-.controller('dynamicModeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+        try {
+          var temp = math.eval($scope.form.query);
+        }
+        catch (e) {
+          console.log(e);
+        }
+        temp = JSON.parse(JSON.stringify(temp));
+        $scope.answer = temp.value + ' ' + temp.unit;
+      }
+    }])
+
+  .controller('dynamicModeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    function($scope, $stateParams) {
 
 
-}])
-   
-.controller('unitModeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    }])
+
+  .controller('unitModeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    function($scope, $stateParams) {
 
 
-}])
- 
+    }])
